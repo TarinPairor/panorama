e4dd4a6664b7bfa97b669cdc634f83304775c3ec
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-12-11 21:07:23 +0800

    Add file upload

--- app/_components/PdfViewer.tsx
+++ app/_components/PdfViewer.tsx
@@ -2,9 +2,9 @@
 "use client";
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
-
 import { pdfjs } from "react-pdf";
-import { useEffect } from "react";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -14,19 +14,39 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const PdfViewer = () => {
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber] = useState<number>(1);
-  useEffect(() => {}, []);
+  const [file, setFile] = useState<File | null>(null);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
+
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const selectedFile = event.target.files?.[0] || null;
+    setFile(selectedFile);
+  }
+
   return (
-    <div>
-      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+    <div className="flex flex-col justify-center items-center">
+      {!file && (
+        <input
+          type="file"
+          accept="application/pdf"
+          onChange={onFileChange}
+          //   className="mb-[700px]"
+        />
+      )}
+      {file && (
+        <div className="mt-96">
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
+      )}
+      {numPages && (
+        <p className="mt-4">
+          Page {pageNumber} of {numPages}
+        </p>
+      )}
     </div>
   );
 };
