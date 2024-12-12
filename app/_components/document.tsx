1c9330a200f1f0578e33c5d140e5a9307461aa1b
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-12-12 23:40:58 +0800

    Abstract document to component

--- app/_components/document.tsx
+++ app/_components/document.tsx
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document as Doc, Page } from "react-pdf";
 
-export default function Document() {
-  return <div>Document</div>;
+interface DocumentProps {
+  file: File;
+}
+
+export default function Document({ file }: DocumentProps) {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  function goToPrevPage() {
+    setPageNumber((prevPageNumber) => Math.max(prevPageNumber - 1, 1));
+  }
+
+  function goToNextPage() {
+    setPageNumber((prevPageNumber) =>
+      Math.min(prevPageNumber + 1, numPages || 1)
+    );
+  }
+
+  return (
+    <div className="flex items-center m-2">
+      <Doc
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        className=""
+        externalLinkRel="noopener noreferrer"
+      >
+        <Page pageNumber={pageNumber} scale={1} />
+      </Doc>
+      <div className="flex flex-col mt-4">
+        <button
+          onClick={goToPrevPage}
+          onMouseEnter={() => console.log("hover")}
+          disabled={pageNumber <= 1}
+          className=""
+        >
+          ⬆️
+        </button>
+        <button
+          onClick={goToNextPage}
+          disabled={pageNumber >= (numPages || 1)}
+          className=""
+        >
+          ⬇️
+        </button>
+      </div>
+      <p className="mt-4 flex">
+        {/* <input
+       type="number"
+       value={pageNumber}
+       onChange={(e) => setPageNumber(parseInt(e.target.value))}
+       className="w-16"
+       step="0.01"
+       height={20}
+     ></input> */}
+        {pageNumber}/{numPages}
+      </p>
+    </div>
+  );
 }
--- app/_components/pdf-viewer.tsx
+++ app/_components/pdf-viewer.tsx
@@ -1,10 +1,10 @@
 // components/PdfViewer.tsx
 "use client";
 import { useState } from "react";
-import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import Document from "./document";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -12,28 +12,11 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const PdfViewer = () => {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
   const [file, setFile] = useState<File | null>(null);
-
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
 
   function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
     const selectedFile = event.target.files?.[0] || null;
     setFile(selectedFile);
-    setPageNumber(1);
-  }
-
-  function goToPrevPage() {
-    setPageNumber((prevPageNumber) => Math.max(prevPageNumber - 1, 1));
-  }
-
-  function goToNextPage() {
-    setPageNumber((prevPageNumber) =>
-      Math.min(prevPageNumber + 1, numPages || 1)
-    );
   }
 
   return (
@@ -44,77 +27,7 @@ const PdfViewer = () => {
         onChange={onFileChange}
         className="mb-4"
       />
-      <div className="">
-        {file ? (
-          <div className="flex items-center m-2">
-            <Document
-              file={file}
-              onLoadSuccess={onDocumentLoadSuccess}
-              className=""
-              externalLinkRel="noopener noreferrer"
-            >
-              <Page pageNumber={pageNumber} scale={1} />
-            </Document>
-            <div className="flex flex-col mt-4">
-              <button
-                onClick={goToPrevPage}
-                onMouseEnter={() => console.log("hover")}
-                disabled={pageNumber <= 1}
-                className=""
-              >
-                ⬆️
-              </button>
-              <button
-                onClick={goToNextPage}
-                disabled={pageNumber >= (numPages || 1)}
-                className=""
-              >
-                ⬇️
-              </button>
-            </div>
-            <p className="mt-4 flex">
-              {/* <input
-                type="number"
-                value={pageNumber}
-                onChange={(e) => setPageNumber(parseInt(e.target.value))}
-                className="w-16"
-                step="0.01"
-                height={20}
-              ></input> */}
-              {pageNumber}/{numPages}
-            </p>
-          </div>
-        ) : (
-          <>
-            <Document
-              file={file}
-              onLoadSuccess={onDocumentLoadSuccess}
-              className=""
-            >
-              <Page pageNumber={1} />
-            </Document>
-            {/* <div className="flex  mt-4">
-              <button
-                onClick={goToPrevPage}
-                disabled={pageNumber <= 1}
-                className="px-4 py-2 bg-gray-300 rounded mr-2"
-              >
-                Previous
-              </button>
-              <button
-                onClick={goToNextPage}
-                disabled={pageNumber >= (numPages || 1)}
-                className="px-4 py-2 bg-gray-300 rounded"
-              >
-                Next
-              </button>
-            </div>
-            <p className="mt-4">
-              Page {pageNumber} of {numPages}
-            </p> */}
-          </>
-        )}
-      </div>
+      <div className="">{file && <Document file={file} />}</div>
     </div>
   );
 };
